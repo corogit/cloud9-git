8af58c467c055d9678c586faa9f44221b3614955
EC2 Default User <ec2-user@ip-172-31-9-205.ec2.internal>
2020-11-08 11:38:47 +0000

    8章

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'cf171708bd2cd1e3e047ce44ae7cb44a6f3a6f236802908c702adf2f188de2d94bfd8280fd9c635079422a2383763c8594cd002778568c199786daf836837a10'
